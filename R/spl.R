de17ffbad89a2aab53ad87737783863deef23bb8
Doug Collinge <doug.collinge@gmail.com>
2017-02-10 19:10:13 -0800

    Revised spl.R Made spl() do what I think is intended in the papers. It also now tries to balance how many times it rounds the split up vs. down.

--- R/spl.R
+++ R/spl.R
@@ -1,7 +1,3 @@
-
-#SUBROUTINE spl(i,Nr,Ni)
-#INTEGER NR,NI(nr+2)
-#
 #' @export
 #' @title spliting interval i into 2 pieces
 #' @param i [INTEGER] interval number, should be less than NR+1
@@ -28,36 +24,44 @@
 #' ni = spl(ni,i=length(ni)-1)
 #' ni
 
-#spl <- function(ni, i) {
-#  if ( length(ni) > 2 ) {
-#    if ( i <= length(ni) - 1 ) {
-#      k1 = ni[i]
-#      k2 = ni[i + 1]
-#      jsplit = max(min(floor((k1 + k2) / 2), k2), k1)
-#      c(ni[1:i], jsplit, ni[(i + 1):length(ni)])
-#    } else {
-#      "i must be less length(ni)-1"
-#    }
-#  }
-#  else {
-#    "ni interval needs to be greater than 2"
-#  }
-#}
-
 spl <- function(ni,i) {
   stopifnot(i < length(ni)) # Otherwise ni[i+1] would fail.
   k1 = ni[i]
   k2 = ni[i+1]
-  jsplit = floor((k1+k2)/2) # Is an index, must be an integer.
+  if ( (k1%%2) == 0) {
+    # k1 is even
+    if ( (k2%%2) == 0) {
+      # k1,k2 are even
+      # no remainder dividing by two
+    } else {
+      # k1 even, k2 odd
+      # adjust k2 to be even
+      k2 = k2 - 1
+    }
+  }
+  else {
+    if ( (k2%%2) == 0) {
+      # k1 odd, k2 even
+      # adjust k1 to be even
+      k1 = k1+1
+    }
+    else {
+      # k1 odd, k2 odd
+      # no adjustments required.
+    }
+  }
+  stopifnot(k2-k1>=2) # Interval too small to split.
 
-  # The following lines are taken from the original code but actually
-  # are in error. They are included simply to make the tests produce the
-  # identical answers in every case.
-  if (jsplit>=k2-1) jsplit=k2-2
-  if (jsplit<=k1+1) jsplit=k1+2
+  jsplit = floor((k1+k2)/2) # Will always come out without a fraction due to adjustments
 
-  stopifnot(k1<jsplit & jsplit<k2) # Verify that the interval has actually been split.
+  # # The following lines are taken from the original code but actually
+  # # are in error. They are included simply to make the tests produce the
+  # # identical answers in every case.
+  # if (jsplit>=k2-1) jsplit=k2-2
+  # if (jsplit<=k1+1) jsplit=k1+2
+#
+#   stopifnot(k1<jsplit & jsplit<k2) # Verify that the interval has actually been split.
+
   # Create a new vector with jsplit at position i
   c( ni[1:i], jsplit, ni[(i+1):length(ni)] )
 }
-
--- tests/testthat/test_spl.R
+++ tests/testthat/test_spl.R
@@ -1,13 +1,13 @@
 context("Testing the spl() function.")
 
 test_that("spl divides wide intervals.", {
-  expect_equal(spl(c(1,5,10,20),2),c(1,5,7,10,20))
+  expect_equal(spl(c(1,5,10,20),2),c(1,5,8,10,20))
   expect_equal(spl(c(1,2,6,10),2),c(1,2,4,6,10))
 })
 
 test_that("spl divides the first and last intervals.", {
   expect_equal(spl(c(1,5,10),1),c(1,3,5,10))
-  expect_equal(spl(c(1,5,10),2),c(1,5,7,10))
+  expect_equal(spl(c(1,5,10),2),c(1,5,8,10))
 })
 
 test_that("spl divides even,even intervals.", {
@@ -15,7 +15,7 @@ test_that("spl divides even,even intervals.", {
 })
 
 test_that("spl divides even,odd intervals.", {
-  expect_equal(spl(c(1,2,5,10),2),c(1,2,4,5,10))
+  expect_equal(spl(c(1,2,5,10),2),c(1,2,3,5,10))
 })
 
 test_that("spl divides odd,even intervals.", {
